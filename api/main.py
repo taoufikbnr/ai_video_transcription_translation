56018b17ef76bc532a15b4e780c83782ee6f3269
taoufikbnr <69259869+taoufikbnr@users.noreply.github.com>
2025-05-31 15:58:03 +0100

    feat:Implemented Whisper for audio transcription

--- api/main.py
+++ api/main.py
@@ -1,10 +1,10 @@
 # main.py
 
 from fastapi import FastAPI
-
-import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 import logging
+import whisper
+from moviepy import VideoFileClip
 
 # Initialize Logger
 logger = logging.getLogger(__name__)
@@ -20,11 +20,25 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
-
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Include routers
+@app.post("/upload")
+async def upload_video():
+    model = whisper.load_model("base")
+
+    # Load and extract audio from video
+    video = VideoFileClip("./welcome.mp4")
+    video.audio.write_audiofile("temp_audio.wav")
+
+    # Transcribe audio
+    result = model.transcribe("temp_audio.wav")
+    print("Transcript:\n", result["text"])
+    
 
 @app.get("/")
 def read_root():
