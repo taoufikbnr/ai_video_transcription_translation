80efc1b23262f284f6583ddfabacba79010026ca
taoufikbnr <69259869+taoufikbnr@users.noreply.github.com>
2025-05-31 16:52:18 +0100

    feat: Implement video upload and transcription processing at backend
    - Added /upload endpoint to handle video file uploads.
    - Implemented asynchronous processing for background video transcription.
    - Background task transcribes audio without blocking the main thread.

--- api/main.py
+++ api/main.py
@@ -1,15 +1,17 @@
 # main.py
 
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+import os
+import uuid
+import asyncio
+from moviepy import VideoFileClip
+
 import logging
 import whisper
-from moviepy import VideoFileClip
 
 # Initialize Logger
 logger = logging.getLogger(__name__)
-
-
 
 app = FastAPI(
     title="AI Video Transcription Transclation",
@@ -26,19 +28,52 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Include routers
-@app.post("/upload")
-async def upload_video():
+
+
+
+jobs = {}
+
+async def process_video(file_path: str, job_id: str):
     model = whisper.load_model("base")
+    try:
+        # Extract audio from video
+        with VideoFileClip(file_path) as video:
+            audio_path = f"uploads/{job_id}_audio.mp3"
+            video.audio.write_audiofile(audio_path)
+        
+        with open(audio_path, "rb"):
+            result = model.transcribe(audio_path)
 
-    # Load and extract audio from video
-    video = VideoFileClip("./welcome.mp4")
-    video.audio.write_audiofile("temp_audio.wav")
+        jobs[job_id] = {
+            "status": "completed",
+            "transcription": result["text"],
+        }
+        
+        os.remove(audio_path)
+        os.remove(file_path)
+        
+    except Exception as e:
+        jobs[job_id] = {"status": "error","error": str(e)}
 
-    # Transcribe audio
-    result = model.transcribe("temp_audio.wav")
-    print("Transcript:\n", result["text"])
+@app.post("/upload")
+async def upload_video(file: UploadFile):
+    if not file.filename.endswith('.mp4'):
+        raise HTTPException(status_code=400, detail="Only MP4 files are allowed")
     
+    job_id = str(uuid.uuid4())
+    file_path = f"uploads/{job_id}.mp4"
+    
+    with open(file_path, "wb") as buffer:
+        content = await file.read()
+        buffer.write(content)
+    
+    jobs[job_id] = {"status": "processing"}
+    
+    asyncio.create_task(process_video(file_path,job_id))
+    
+    return {"job_id": job_id}
+
+
 
 @app.get("/")
 def read_root():
